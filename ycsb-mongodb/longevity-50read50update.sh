0a4e2caad2f7ee39d47c892c79f8becadecbdc03
Chung-Yen Chang <chung-yen.chang@10gen.com>
2015-11-03 01:09:02 -0800

    Pass additional parameters to YCSB 50/50 longevity

--- ycsb-mongodb/longevity-50read50update.sh
+++ ycsb-mongodb/longevity-50read50update.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
 SERVER=$1
+shift
 
-./bin/ycsb run mongodb -s -P workloads/workloadLongevity -p mongodb.url=$SERVER:27017 -threads 64 
+# build YCSB command line with arguments passed in from command line
+cmdline="./bin/ycsb run mongodb -s -P workloads/workloadLongevity -p mongodb.url=$SERVER:27017 -threads 64 "
+while [[ $# > 0 ]]
+do
+    cmdline="$cmdline -p $1"
+    shift
+done
+
+$cmdline
+
+
