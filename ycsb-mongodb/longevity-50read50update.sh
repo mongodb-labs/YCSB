426ac98d91d03388ef67ed979ce360ae3c18f87f
Chung-Yen Chang <chung-yen.chang@10gen.com>
2015-11-04 20:36:29 -0800

    Restore longevity-50read50update.sh

--- ycsb-mongodb/longevity-50read50update.sh
+++ ycsb-mongodb/longevity-50read50update.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
 SERVER=$1
-shift
 
-# build YCSB command line with arguments passed in from command line
-cmdline="./bin/ycsb run mongodb -s -P workloads/workloadLongevity -p mongodb.url=$SERVER:27017 -threads 64 "
-while [[ $# > 0 ]]
-do
-    cmdline="$cmdline -p $1"
-    shift
-done
-
-$cmdline
-
-
+./bin/ycsb run mongodb -s -P workloads/workloadLongevity -p mongodb.url=$SERVER:27017 -threads 64 
