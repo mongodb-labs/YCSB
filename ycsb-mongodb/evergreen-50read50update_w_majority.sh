492568781730bce68fcce9f91204e477e53a578d
Chung-Yen Chang <chung-yen.chang@10gen.com>
2015-11-11 22:59:25 -0800

    PERF-273: Set w_majority YCSB max time to 10 min

--- ycsb-mongodb/evergreen-50read50update_w_majority.sh
+++ ycsb-mongodb/evergreen-50read50update_w_majority.sh
@@ -2,4 +2,4 @@
 
 SERVER=$1
 
-./bin/ycsb run  mongodb -s -P workloads/workloadEvergreen_50read50update -p mongodb.url="mongodb://$SERVER:27017/ycsb?w=majority" -threads 64
+./bin/ycsb run  mongodb -s -P workloads/workloadEvergreen_50read50update -p mongodb.url="mongodb://$SERVER:27017/ycsb?w=majority" -threads 64 -p maxexecutiontime=600
