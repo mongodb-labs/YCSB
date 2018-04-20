d0bc14b2004c005738fb24baff2085ca8a9ed6a1
Ben Caimano <ben.caimano@10gen.com>
2018-04-20 16:22:59 -0400

    Changed tabs to spaces, standardized line endings

--- ycsb-mongodb/core/src/main/java/com/yahoo/ycsb/generator/FileGenerator.java
+++ ycsb-mongodb/core/src/main/java/com/yahoo/ycsb/generator/FileGenerator.java
@@ -1,16 +1,16 @@
-/**                                                                                                                                                                                
- * Licensed under the Apache License, Version 2.0 (the "License"); you                                                                                                             
- * may not use this file except in compliance with the License. You                                                                                                                
- * may obtain a copy of the License at                                                                                                                                             
- *                                                                                                                                                                                 
- * http://www.apache.org/licenses/LICENSE-2.0                                                                                                                                      
- *                                                                                                                                                                                 
- * Unless required by applicable law or agreed to in writing, software                                                                                                             
- * distributed under the License is distributed on an "AS IS" BASIS,                                                                                                               
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or                                                                                                                 
- * implied. See the License for the specific language governing                                                                                                                    
- * permissions and limitations under the License. See accompanying                                                                                                                 
- * LICENSE file.                                                                                                                                                                   
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You
+ * may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License. See accompanying
+ * LICENSE file.
  */
 
 package com.yahoo.ycsb.generator;
@@ -26,63 +26,63 @@ import java.io.IOException;
  */
 public class FileGenerator extends Generator
 {
-	String filename;
-	String current;
-	BufferedReader reader;
+    String filename;
+    String current;
+    BufferedReader reader;
 
-	/**
-	 * Create a FileGenerator with the given file.
-	 * @param _filename The file to read lines from.
-	 */
-	public FileGenerator(String _filename)
-	{
-		try {
-			filename = _filename;
-			File file = new File(filename);
-			FileInputStream in = new FileInputStream(file);
-			reader = new BufferedReader(new InputStreamReader(in));
-		} catch(IOException e) {
-			System.err.println("Exception: " + e);
-		}
-	}
+    /**
+     * Create a FileGenerator with the given file.
+     * @param _filename The file to read lines from.
+     */
+    public FileGenerator(String _filename)
+    {
+        try {
+            filename = _filename;
+            File file = new File(filename);
+            FileInputStream in = new FileInputStream(file);
+            reader = new BufferedReader(new InputStreamReader(in));
+        } catch(IOException e) {
+            System.err.println("Exception: " + e);
+        }
+    }
 
-	/**
-	 * Return the next string of the sequence, ie the next line of the file.
-	 */
-	public synchronized String nextString()
-	{
-		try {
-			return current = reader.readLine();
-		} catch(NullPointerException e) {
-			System.err.println("NullPointerException: " + filename + ':' + current);
-			throw e;
-		} catch(IOException e) {
-			System.err.println("Exception: " + e);
-			return null;
-		}
-	}
+    /**
+     * Return the next string of the sequence, ie the next line of the file.
+     */
+    public synchronized String nextString()
+    {
+        try {
+            return current = reader.readLine();
+        } catch(NullPointerException e) {
+            System.err.println("NullPointerException: " + filename + ':' + current);
+            throw e;
+        } catch(IOException e) {
+            System.err.println("Exception: " + e);
+            return null;
+        }
+    }
 
-	/**
-	 * Return the previous read line.
-	 */
-	public String lastString()
-	{
-		return current;
-	}
+    /**
+     * Return the previous read line.
+     */
+    public String lastString()
+    {
+        return current;
+    }
 
-	/**
-	 * Reopen the file to reuse values.
-	 */
-	public synchronized void reloadFile()
-	{
-		try {
-			System.err.println("Reload " + filename);
-			reader.close();
-			File file = new File(filename);
-			FileInputStream in = new FileInputStream(file);
-			reader = new BufferedReader(new InputStreamReader(in));
-		} catch(IOException e) {
-			System.err.println("Exception: " + e);
-		}
-	}
+    /**
+     * Reopen the file to reuse values.
+     */
+    public synchronized void reloadFile()
+    {
+        try {
+            System.err.println("Reload " + filename);
+            reader.close();
+            File file = new File(filename);
+            FileInputStream in = new FileInputStream(file);
+            reader = new BufferedReader(new InputStreamReader(in));
+        } catch(IOException e) {
+            System.err.println("Exception: " + e);
+        }
+    }
 }
